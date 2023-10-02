575c61b54acd26fc40600e882187a7bda8b5afff
HussienSayed14 <hussiens399@gmail.com>
2023-10-03 00:28:50 +0300

    Spring Security, JWT Authentication and Authorization and JWT Session Added

--- backend/springboot-MultiClinic/src/main/java/com/multiClinic/springbootMultiClinic/doctor/Doctor.java
+++ backend/springboot-MultiClinic/src/main/java/com/multiClinic/springbootMultiClinic/doctor/Doctor.java
@@ -3,18 +3,23 @@ package com.multiClinic.springbootMultiClinic.doctor;
 import com.multiClinic.springbootMultiClinic.patient.Patient;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table
-public class Doctor {
+public class Doctor implements UserDetails {
 
     @Id
     @GeneratedValue
@@ -42,4 +47,41 @@ public class Doctor {
         }
         return searchedPatients;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+
+
 }
---
+++ backend/springboot-MultiClinic/src/main/java/com/multiClinic/springbootMultiClinic/doctor/security/auth/AuthenticationController.java
@@ -0,0 +1,30 @@
+package com.multiClinic.springbootMultiClinic.doctor.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+
+    private final AuthenticationService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponses> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody AuthenticationRequest request) {
+        return authService.authenticate(request);
+
+    }
+
+}
+
